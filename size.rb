41eeb6bbcd1dad4888c6d3a637d6ebc466c32723
Masato Ikeda <masato.ikeda@gmail.com>
2018-03-25 22:44:20 +0900

    format

--- size.rb
+++ size.rb
@@ -2,13 +2,15 @@
 
 $LOAD_PATH << File.join(__dir__, "ruby")
 
+count = (ARGV[0] || 100).to_i
+
 require "json"
 require "msgpack"
 
 require_relative "ruby/person_message_pb"
 require_relative "ruby/group_message_pb"
 
-people = (1..100).map do |i|
+people = (1..count).map do |i|
   PersonMessage.new(id: i,
                     name: "##{i}",
                     email: "foo.bar#{i}@example.com")
@@ -17,21 +19,21 @@ end
 group = GroupMessage.new(people: people)
 
 protobuf = GroupMessage.encode(group)
-puts "protobuf: #{protobuf.bytesize}"
+puts "protobuf	#{protobuf.bytesize}"
 
 json = GroupMessage.encode_json(group)
-puts "json: #{json.bytesize}"
+puts "json	#{json.bytesize}"
 
 data = JSON.parse(json)
 
 msgpack = MessagePack.dump(data)
-puts "msgpack: #{msgpack.bytesize}"
+puts "msgpack	#{msgpack.bytesize}"
 
 cols = people.first.to_hash.keys
 slim_people = [cols.size] + cols
 people.each do |person|
   slim_people += person.to_hash.values
 end
-slim_data = { group: slim_people }
+slim_data = { group:	slim_people }
 slim_msgpack = MessagePack.dump(slim_data)
-puts "slim msgpack: #{slim_msgpack.bytesize}"
+puts "slim msgpack	#{slim_msgpack.bytesize}"
