eaaad3b3856b716a070fe89c1939c95f591844c2
Masato Ikeda <masato.ikeda@gmail.com>
2018-03-25 21:56:45 +0900

    size benchmark

---
+++ benchmark.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+$LOAD_PATH << File.join(__dir__, "ruby")
+
+require_relative "ruby/person_message_pb"
+require_relative "ruby/group_message_pb"
+
+people = (1..100).map do |i|
+  PersonMessage.new(id: i,
+                    name: "##{i}",
+                    email: "foo.bar#{i}@example.com")
+end
+
+group = GroupMessage.new(people: people)
+
+protobuf = GroupMessage.encode(group)
+puts protobuf.bytesize
+
+json = GroupMessage.encode_json(group)
+puts json.bytesize
